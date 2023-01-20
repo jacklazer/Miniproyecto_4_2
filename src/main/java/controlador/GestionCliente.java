353691c1e5ba285ff875e8a42791c16406e91d84
Alejandra Carvajal <maria.alejandra.carvajal@correounivalle.edu.co>
2023-01-20 16:08:06 -0500

    Uhih

--- src/main/java/controlador/GestionCliente.java
+++ src/main/java/controlador/GestionCliente.java
@@ -4,11 +4,51 @@
  */
 package controlador;
 
+import java.util.ArrayList;
+import modelo.Cliente;
+
 
 /**
  *
  * @author aleja
  */
-public class GestionCliente {
+public class GestionCliente implements IGestionDatos{
+
+        private ArrayList <Cliente> listaClientes;
+    
+    
+//    public GestionCliente(){
+//        listaClientes = new ArrayList();
+//    }
+    
+    @Override
+    public void agregar() {
+    
+    }
+
+    @Override
+    public void actualizar() {
+    
+    }
+
+    @Override
+    public void eliminar() {
+    
+    }
+
+    @Override
+    public String listar() {
+    
+    return "holi";}
+
+    @Override
+    public void generarCSV() {
+    
+    }
+
+    @Override
+    public void restaurarDatos() {
+    
+    }
     
 }
