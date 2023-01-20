33266cae3793c07d046d02e9680aeee2fb3eb811
Alejandra Carvajal <maria.alejandra.carvajal@correounivalle.edu.co>
2023-01-20 15:39:35 -0500

    cliente

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -12,5 +12,52 @@ package modelo;
  */
 
 public class Cliente {
+ 
     
+    private final Integer cedula;
+    private String nombres;
+    private String apellidos;
+    private Integer tarjetaDeCredito;
+
+    public Cliente(Integer cedula, String nombres, String apellidos, Integer tarjetaDeCredito) {
+        this.cedula = cedula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.tarjetaDeCredito = tarjetaDeCredito;
+    }
+
+    public Integer getCedula() {
+        return cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getTarjetaDeCredito() {
+        return tarjetaDeCredito;
+    }
+
+    public void setTarjetaDeCredito(Integer tarjetaDeCredito) {
+        this.tarjetaDeCredito = tarjetaDeCredito;
+    }
+    
+    @Override
+    public String toString() {
+        return "Cliente{" + "cedula=" + this.getCedula() + ", nombres=" + this.getNombres() +
+        ", apellidos=" + this.getApellidos() + ", tarjeta de credito=" + this.getTarjetaDeCredito() + '}';
+    }
+
 }
